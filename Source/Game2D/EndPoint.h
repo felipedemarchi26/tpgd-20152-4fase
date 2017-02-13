c842c6b209b15c16695b495e5ddc59bfcf954c61
Felipe Demarchi <felipe.demarchi@edu.sc.senai.br>
2017-02-13 14:18:51 -0200

    Criação da classe EndPoint

---
+++ Source/Game2D/EndPoint.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "EndPoint.generated.h"
+
+UCLASS()
+class GAME2D_API AEndPoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEndPoint();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sprite", meta = (AllowPrivateAccess = "true"))
+		class UPaperSpriteComponent* Sprite;
+	
+};
